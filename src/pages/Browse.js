26450c06721e189b945ef62d7567c63d62f53d18
craigmcg <craigmcgowan15@gmail.com>
2022-01-08 08:21:46 -0500

    minor browse styling

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -87,79 +87,84 @@ const Browse = (props) => {
   return names ? (
     <div className="flex  ">
       {names?.map((name) => (
-        <BrowseCard thisName={name} createFav={createFav}/>
+        <BrowseCard thisName={name} createFav={createFav} />
       ))}
       <div className="w-60 h-60 fixed left-[40%] top-[33%] flex flex-wrap align-center justify-around content-center">
-      <button
-        className="orange-btn w-2/3 h-1/4 mb-4"
-        onClick={(e) => {
-          setFormData(null);
-          setNames(null);
-        }}
+        <button
+          className="orange-btn w-2/3 h-1/4 mb-4"
+          onClick={(e) => {
+            setFormData(null);
+            setNames(null);
+          }}
         >
-        Try Another Category
-      </button>
-      <button
-        className="green-btn w-2/3 h-1/4"
-        onClick={(e) => getNames(e)}
-        >
-        Keep Going
-      </button>
+          Try Another Category
+        </button>
+        <button className="green-btn w-2/3 h-1/4" onClick={(e) => getNames(e)}>
+          Keep Going
+        </button>
       </div>
     </div>
   ) : (
-    <div className="flex justify-center w-screen pt-10">
-      <h1>Choose Your Your Categories to Start Browsing</h1>
-        <form
+    <div className="flex flex-wrap justify-center w-screen pt-10">
+      <h2 className="w-[60%] text-2xl my-5">
+        Choose Your Your Categories to Start Browsing
+      </h2>
+      <form
         className="w-[60%]"
         onSubmit={(e) => {
           getNames(e);
         }}
-        >
+      >
         <fieldset>
-        <legend>Gender</legend>    
-        <FormField
-          type="radio"
-          onChange={handleChange}
-          value="m"
-          name="gender"
-          label="Masculine"
-          required
+          <legend className="text-xl text-indigo-800 mb-4">
+            Gender
+          </legend>
+          <FormField
+            type="radio"
+            onChange={handleChange}
+            value="m"
+            name="gender"
+            label="Masculine"
+            required
           />
-        <FormField
-          type="radio"
-          onChange={handleChange}
-          value="f"
-          name="gender"
-          label="Feminine"
-          required
+          <FormField
+            type="radio"
+            onChange={handleChange}
+            value="f"
+            name="gender"
+            label="Feminine"
+            required
           />
-        <FormField
-          type="radio"
-          onChange={handleChange}
-          value="u"
-          name="gender"
-          label="Neutral"
-          required
+          <FormField
+            type="radio"
+            onChange={handleChange}
+            value="u"
+            name="gender"
+            label="Neutral"
+            required
           />
-          </fieldset>
-        {origins.map((continent) => (
-          <details className="rounded-lg open:bg-gray-100">
-            <summary className=" cursor-pointer hover:scale-110 list-none ">
-              {continent.continent}
-            </summary>
-            {continent.languageArr.map(({ searchKey, language }) => (
-              <FormField
-                type="radio"
-                onChange={handleChange}
-                name="origin"
-                value={searchKey + "%%%" + language}
-                label={language}
-                required
-              />
-            ))}
-          </details>
-        ))}
+        </fieldset>
+        <fieldset className="my-5">
+          <label className=" text-xl text-indigo-800 mb-6">Name Origin</label>
+
+          {origins.map((continent) => (
+            <details className="rounded-lg open:bg-gray-100">
+              <summary className=" mt-2 cursor-pointer hover:scale-110 list-none ">
+                {continent.continent}
+              </summary>
+              {continent.languageArr.map(({ searchKey, language }) => (
+                <FormField
+                  type="radio"
+                  onChange={handleChange}
+                  name="origin"
+                  value={searchKey + "%%%" + language}
+                  label={language}
+                  required
+                />
+              ))}
+            </details>
+          ))}
+        </fieldset>
         <input
           type="submit"
           value="Start"
